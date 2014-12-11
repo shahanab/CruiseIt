2c60868a472491f20eff5c791de1c35fe9b72369
Jessica Hunt <JessicaHunt@Jessicas-MacBook-Pro.local>
2014-12-10 19:44:41 -0500

    added TA activities

--- app/models/tripadvisor.rb
+++ app/models/tripadvisor.rb
@@ -12,7 +12,15 @@ class Tripadvisor
     restaurants.each do |restaurant|
       Poi.find_or_create_by(description: restaurant["name"])
     end
+  end
+    def get_activities
+      @@apikey = '6928d6617f794b0894f6cea24606ce1c'
+      response = HTTParty.get("http://api.tripadvisor.com/api/partner/2.0/location/147358/attractions?key=#{@@apikey}")
+      activities = JSON.parse(response.body)["data"]
 
+      activities.each do |activities|
+      Poi.find_or_create_by(description: activities["name"])
+    end
     # parse[0]["name"]
     # parse[0]["address_obj"]
     # parse[0]["rating"]
