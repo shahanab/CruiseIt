9c4afa3c932fee4ff330332f46a2605ab2dee906
Jessica Hunt <JessicaHunt@Jessicas-MacBook-Pro.local>
2014-12-12 11:36:40 -0500

    added tortola restaurants and activities

--- app/models/tripadvisor.rb
+++ app/models/tripadvisor.rb
@@ -21,9 +21,26 @@ class Tripadvisor
       activities.each do |activities|
       Poi.find_or_create_by(description: activities["name"])
     end
-    # parse[0]["name"]
-    # parse[0]["address_obj"]
-    # parse[0]["rating"]
+  end
+    
+  def get_restaurants_tortola
+      @@apikey = '6928d6617f794b0894f6cea24606ce1c'
+      response = HTTParty.get("http://api.tripadvisor.com/api/partner/2.0/location/147354/restaurants?key=#{@@apikey}") 
+      restaurants = JSON.parse(response.body)["data"]
+
+    restaurants.each do |restaurant|
+      Poi.find_or_create_by(description: restaurant["name"])
+    end
+  end
+
+  def get_activities_tortola
+      @@apikey = '6928d6617f794b0894f6cea24606ce1c'
+      response = HTTParty.get("http://api.tripadvisor.com/api/partner/2.0/location/147354/attractions?key=#{@@apikey}")
+      activities = JSON.parse(response.body)["data"]
+
+      activities.each do |activities|
+      Poi.find_or_create_by(description: activities["name"])
+    end
   end
 end
 
