8d3852e6fc84f5810c795acea84720ac7cfa6e25
Jessica Hunt <JessicaHunt@Jessicas-MacBook-Pro.local>
2014-12-13 17:13:16 -0500

    tripadvisor activites and restaurant

--- app/models/tripadvisor.rb
+++ app/models/tripadvisor.rb
@@ -23,8 +23,15 @@ class Tripadvisor
       response = HTTParty.get("http://api.tripadvisor.com/api/partner/2.0/location/147358/attractions?key=#{@@apikey}")
       activities = JSON.parse(response.body)["data"]
 
-      activities.each do |activities|
-      Poi.find_or_create_by(description: activities["name"])
+      activities.each do |activity|
+      p = Poi.find_or_create_by(description: activity["name"])
+      p.rating = activity["rating"] 
+      p.address = "#{activity['address_obj']['street1']} #{activity['address_obj']['street2']}
+#{activity["address_obj"]["city"]}, #{activity["address_obj"]["state"]}
+#{activity["address_obj"]["country"]} #{activity["address_obj"]["postalcode"]}"
+      p.save
+
+
     end
   end
   def self.get_restaurants_tortola
@@ -33,7 +40,12 @@ class Tripadvisor
       restaurants = JSON.parse(response.body)["data"]
 
     restaurants.each do |restaurant|
-      Poi.find_or_create_by(description: restaurant["name"])
+      p = Poi.find_or_create_by(description: restaurant["name"])
+      p.rating = restaurant["rating"] 
+      p.address = "#{restaurant['address_obj']['street1']} #{restaurant['address_obj']['street2']}
+#{restaurant["address_obj"]["city"]}, #{restaurant["address_obj"]["state"]}
+#{restaurant["address_obj"]["country"]} #{restaurant["address_obj"]["postalcode"]}"
+      p.save
     end
   end
   def self.get_activities_tortola
@@ -41,8 +53,15 @@ class Tripadvisor
       response = HTTParty.get("http://api.tripadvisor.com/api/partner/2.0/location/147354/attractions?key=#{@@apikey}")
       activities = JSON.parse(response.body)["data"]
 
-      activities.each do |activities|
-      Poi.find_or_create_by(description: activities["name"])
+       activities.each do |activity|
+      p = Poi.find_or_create_by(description: activity["name"])
+      p.rating = activity["rating"] 
+      p.address = "#{activity['address_obj']['street1']} #{activity['address_obj']['street2']}
+#{activity["address_obj"]["city"]}, #{activity["address_obj"]["state"]}
+#{activity["address_obj"]["country"]} #{activity["address_obj"]["postalcode"]}"
+      p.save
+
+
     end
   end
   def self.populate
